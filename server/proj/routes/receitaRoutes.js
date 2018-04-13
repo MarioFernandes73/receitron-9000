3d31273627e4840bc2c545e3846c28c5f8fea7fb
Miguel Freitas <miguel.agfreitas@gmail.com>
2018-04-13 23:13:28 +0100

    Pagination of receitas

--- server/proj/routes/receitaRoutes.js
+++ server/proj/routes/receitaRoutes.js
@@ -17,4 +17,14 @@ router.get('/:user_id', function (req, res) {
 
 });
 
+router.get('/page/:page', function(req, res){
+    let page = req.params["page"];
+    Receita.paginate({}, { page: page, limit: 10 }, function(err, result) {
+        if(err){
+            return res.send(error);
+        }
+        return res.send(result);
+    });
+})
+
 module.exports = router;
