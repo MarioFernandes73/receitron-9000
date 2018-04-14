57a12baa949aaa5de82c37f46a028709b84adb30
Unknown <catramos96@gmail.com>
2018-04-14 01:35:07 +0100

    Home page

--- client_t/src/app/pages/starter/starter.module.ts
+++ client_t/src/app/pages/starter/starter.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
 import { StarterComponent } from './starter.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 
 const routes: Routes = [{
@@ -18,7 +19,9 @@ const routes: Routes = [{
 @NgModule({
 	imports: [
     	FormsModule,
-    	CommonModule, 
+		CommonModule, 
+		MatFormFieldModule,
+		MatInputModule,
     	RouterModule.forChild(routes)
     ],
 	declarations: [StarterComponent]
