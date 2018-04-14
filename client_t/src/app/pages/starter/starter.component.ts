866212ebecea8a30f7ec82660c603433d3c6d8ca
inesgomes <gomes.inesisabel@gmail.com>
2018-04-14 02:20:21 +0100

    html da pagina de descricao acabado

--- client_t/src/app/pages/starter/starter.component.ts
+++ client_t/src/app/pages/starter/starter.component.ts
@@ -1,12 +1,18 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
 	templateUrl: './starter.component.html'
 })
 export class StarterComponent implements AfterViewInit {
 	subtitle:string;	
-	constructor() {
+	constructor(public router: Router) {
 		this.subtitle = "This is some text within a card block."
 	}
 
 	ngAfterViewInit(){}
+
+	goToDesc(){
+		//vai receber um id
+        this.router.navigate(['/description']);
+	}
 }
